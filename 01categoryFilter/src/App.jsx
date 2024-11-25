f500053fc24669dcd5fde4922ed6659e6bf74bf2
Tuhin Ghosh <tuhinkatwa8@gmail.com>
2024-11-25 17:23:48 +0530

    react-project

--- 01categoryFilter/src/App.jsx
+++ 01categoryFilter/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import Category from "./components/Category"
+import { useState } from "react";
+import "./App.css";
+import Category from "./components/Category";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Category />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
