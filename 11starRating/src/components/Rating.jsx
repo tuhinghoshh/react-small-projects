53cab446ee014f8f1ffe49eddb31433ae3dc078b
Tuhin Ghosh <tuhinkatwa8@gmail.com>
2024-12-01 00:17:47 +0530

    react-project

---
+++ 11starRating/src/components/Rating.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function Rating() {
+  const [rating, setRating] = useState();
+  const [hover, setHover] = useState(0);
+
+  let arr = new Array(5).fill(0);
+  return (
+    <div>
+      <div className="head">
+        <h1>Star Rating</h1>
+        <p>Please Rate Us</p>
+      </div>
+      <div className="container">
+        {arr.map((curValue, index) => {
+          return (
+            <span
+              className={
+                (hover == 0 && index < rating) || index < hover
+                  ? "colored"
+                  : "unColor"
+              }
+              onClick={() => setRating(index + 1)}
+              onMouseEnter={() => setHover(index + 1)}
+              onMouseLeave={() => setHover(0)}
+              key={index}
+            >
+              &#9733;
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Rating;
