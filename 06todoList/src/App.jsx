e95bf4802f717bbffe364148b45acc191ce6f7fe
Tuhin Ghosh <tuhinkatwa8@gmail.com>
2024-12-15 23:41:18 +0530

    react-project

--- 06todoList/src/App.jsx
+++ 06todoList/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import Todos from "./components/Todos"
+import { useState } from "react";
+import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Todos />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
