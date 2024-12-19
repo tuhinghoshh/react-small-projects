475ea455181f051b7df1d7f5009305e99ecfbe2e
Tuhin Ghosh <tuhinkatwa8@gmail.com>
2024-12-19 23:03:31 +0530

    react-project

--- 07infiniteScroll/src/App.jsx
+++ 07infiniteScroll/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import InfiniteScroll from "./components/InfiniteScroll"
+import { useState } from "react";
+import "./App.css";
+import InfiniteScroll from "./components/InfiniteScroll";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <InfiniteScroll />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
